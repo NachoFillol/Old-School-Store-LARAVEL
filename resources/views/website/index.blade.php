cd44c1634ce3cc42fb5bc9545b5f4e8f40c914b0
augustomauro <55675540+augustomauro@users.noreply.github.com>
2020-01-27 21:25:37 -0300

    Version 27-01-2020 - 21;00hs

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -22,34 +22,25 @@
                         
                         <!-- Indicators -->
                         <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+
+                            @foreach($products as $product)
+                            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="<?= $loop->first ? 'active':null ?>"></li>
+                            @endforeach
+
                         </ol>
 
                         <!-- Wrapper for slides -->
                         <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="img/ejemplo1.png" class="d-block w-100" alt="...">
+
+                            @foreach($products as $product)
+                            <div class="carousel-item <?= $loop->first ? 'active':null ?>">
+                                <img src="{{ asset($product->image) }}" width=500px class="d-block w-100" alt="{{ $product->name }}">
                                 <div class="carousel-caption d-none d-md-block">
-                                <h5>SEGA Mega Drive</h5>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                    <h5>{{ $product->name }}</h5>
                                 </div>
                             </div>
-                            <div class="carousel-item">
-                                <img src="img/ejemplo2.png" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                <h5>Atari</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img/ejemplo3.png" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                <h5>Camara Polaroid</h5>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                </div>
-                            </div>
+                            @endforeach
+                         
                         </div>
 
                         <!-- Controls -->
